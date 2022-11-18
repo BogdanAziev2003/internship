8ad7aa2be375c7c14794990bd894f82cf7d50fa3
BogdanAziev2003 <neazievbogdan@gmail.com>
2022-11-18 18:03:11 +0300

    news-swiper almost

--- js/swiper.js
+++ js/swiper.js
@@ -1,23 +1,19 @@
 let swiper = new Swiper(".swiper", {
 
-    slidesPerView: "auto",
-    freeMod: true, 
-    a11y: true,
-    keyboardControl: true,
-    grabCursor: true,
+  slidesPerView: 'auto',
+  keyboardControl: true,
+  grabCursor: true,
+  loop:true,
+  spaceBetween: 32,
+  navigation: {
+      nextEl: '.news__navigation-right-arrow',
+      prevEl: '.news__navigation-left-arrow',
+    },
 
-    navigation: {
-        nextEl: 'news__navigation-right-arrow',
-        prevEl: 'news__navigation-left-arrow',
-      },
-  });
-
-
-  function prevSlide(){
-    navigationNext(swiper);
+    on: {
+  slideChange: function(swiper){
+    document.querySelector('.news__nvigation-count_active').innerHTML = (swiper.realIndex <10?"0":'') + (+swiper.realIndex+1);
   }
+}
+});
 
-  function nextSlide(){
-    navigationNext(swiper);
-
-  }
